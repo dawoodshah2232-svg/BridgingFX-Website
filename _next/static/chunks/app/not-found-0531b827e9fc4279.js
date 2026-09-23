4094750eab100cd0256cfd76a7e4533ccf98e523
Quinn <quinn@bridgingfx.local>
2026-09-23 21:24:39 +0400

    BridgingFX v2 preview build (static export for GitHub Pages)

---
+++ _next/static/chunks/app/not-found-0531b827e9fc4279.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{6959:function(n,e,u){Promise.resolve().then(u.t.bind(u,98,23))}},function(n){n.O(0,[98,521,298,744],function(){return n(n.s=6959)}),_N_E=n.O()}]);
